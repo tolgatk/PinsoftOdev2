0c75b883c8410dc4c8282eb6a681481965012358
Tolga <tolgatok34@hotmail.com>
2024-01-04 15:21:17 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int kenar1=10,kenar2=20;
+        System.out.println("Dikdörtgenin alanı = " + (kenar1 * kenar2));
+        System.out.println("Dikdörtgenin çevresi = " + (2 * (kenar1 + kenar2)));
+
+
+    }
+
+}
